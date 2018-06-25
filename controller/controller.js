8dacb23ce30b9aac5318a32e70f7e96ed6bc8b5a
Roy Springer <r.springer@rocmondriaan.nl>
2018-06-25 17:30:34 +0200

    Opdracht 1 student

--- controller/controller.js
+++ controller/controller.js
@@ -1,40 +1,18 @@
 class Controller {
   constructor() {
     this.klokModel = new KlokModel();
-    this.klokView = new KlokView(this.klokModel);
-    this.klokViewAnalog = new AnalogClockView(this.klokModel);
-    this.timeTableView = new TimeTableView(this.klokModel);
-    this.timerID = -1;
 
-    document.getElementById("start").addEventListener("click", (e) => {
-      console.log("Start");
-      this.startKlok();
-    })
-    document.getElementById("stop").addEventListener("click", (e) => {
-      console.log("Stop");
-      this.stopKlok();
-    })
-    document.getElementById("save").addEventListener("click", (e) => {
-      console.log("Save");
-      this.saveKlok();
-    })
   }
 
   startKlok() {
-    this.timerID = setInterval(this.updateKlok.bind(this), 1000);
+  
   }
 
   stopKlok() {
-    clearInterval(this.timerID);
-  }
-
-  saveKlok() {
-    this.klokModel.saveTime();
+  
   }
 
   updateKlok() {
-    console.log("Update clock");
-    this.klokModel.updateTime();
-  }
   
+  }
 }
--- model/klokmodel.js
+++ model/klokmodel.js
@@ -1,28 +1,10 @@
-class KlokModel extends Observable {
+class KlokModel {
   constructor() {
-    super();
     this.time = new Date();
-    this.id = "stopWatchKlok"
   }
 
   updateTime() {
     this.time = new Date();
-    this.notify();
-  }
-
-  saveTime() {
-    let saveObject = {
-      time: this.time
-    }
-    let timeArray = JSON.parse(localStorage.getItem(this.id)) || [];
-    timeArray.push(saveObject);
-    localStorage.setItem(this.id, JSON.stringify(timeArray));
-    this.notify();
-  }
-  
-  getTimeTable() {
-    let timeArray = JSON.parse(localStorage.getItem(this.id)) || [];
-    return timeArray;
   }
 
   getHours() {
